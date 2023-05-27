d8599ad4c0946db35339e24eee5346dedb4bc53d
4ndferreira <andferreira.art@outlook.com>
2023-05-27 17:46:49 -0300

    Renamed files

---
+++ data-display.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function showSavedData() {
+      let formDataJson = localStorage.getItem("formData");
+  
+      if (formDataJson) {
+        let formData = JSON.parse(formDataJson);
+  
+        let name = formData.name;
+        let email = formData.email;
+        let message = formData.message;
+        let interests = formData.interests;
+  
+        document.getElementById("name").textContent = name;
+        document.getElementById("email").textContent = email;
+        document.getElementById("message").textContent = message;
+        document.getElementById("interests").textContent = interests.join(", ");
+      }
+    }
+  
+    showSavedData();
+  });
